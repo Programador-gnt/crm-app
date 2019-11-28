c5b3cc6485a7695ff4361857cdfbb6a03a2ca744
programador-gnt <programador@gnt.pe>
2019-11-28 09:40:14 -0500

    Updates

---
+++ precache-manifest.08d264f720584e87dd8e566500f035bb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243bee12bc6a800ffb3e92a1c5f00c51",
+    "url": "/crm-app/index.html"
+  },
+  {
+    "revision": "c371666eff42670916f9",
+    "url": "/crm-app/static/css/6.54016b47.chunk.css"
+  },
+  {
+    "revision": "d93331d78605375fc792",
+    "url": "/crm-app/static/css/main.d932c3b8.chunk.css"
+  },
+  {
+    "revision": "c5855a1d1561e556a945",
+    "url": "/crm-app/static/js/0.769f8f24.chunk.js"
+  },
+  {
+    "revision": "5c2f1cab535f5dc5239f",
+    "url": "/crm-app/static/js/1.97dfaf5e.chunk.js"
+  },
+  {
+    "revision": "703fcb9029ad8bfb477a",
+    "url": "/crm-app/static/js/10.5b26cf95.chunk.js"
+  },
+  {
+    "revision": "e6888c01d9ea3a9234e9",
+    "url": "/crm-app/static/js/11.2c98aef0.chunk.js"
+  },
+  {
+    "revision": "341f920b2cd8e5f59a97",
+    "url": "/crm-app/static/js/12.5aca8c22.chunk.js"
+  },
+  {
+    "revision": "b2247e54dbd4bce08d6d",
+    "url": "/crm-app/static/js/13.a0b3a45a.chunk.js"
+  },
+  {
+    "revision": "50edab02ac7815d86bd3",
+    "url": "/crm-app/static/js/14.d60dd535.chunk.js"
+  },
+  {
+    "revision": "6fee2a46185cb2028bfc",
+    "url": "/crm-app/static/js/2.00a4bd0f.chunk.js"
+  },
+  {
+    "revision": "ceb3159efe27624a4231",
+    "url": "/crm-app/static/js/5.23d6ab0c.chunk.js"
+  },
+  {
+    "revision": "c371666eff42670916f9",
+    "url": "/crm-app/static/js/6.5f038cde.chunk.js"
+  },
+  {
+    "revision": "3c017aee7570b91cebbf",
+    "url": "/crm-app/static/js/7.9a5d1372.chunk.js"
+  },
+  {
+    "revision": "1990be17a61311140642",
+    "url": "/crm-app/static/js/8.abba407b.chunk.js"
+  },
+  {
+    "revision": "57c548da1aab80f36b1e",
+    "url": "/crm-app/static/js/9.cb9ea90d.chunk.js"
+  },
+  {
+    "revision": "d93331d78605375fc792",
+    "url": "/crm-app/static/js/main.8aa38a38.chunk.js"
+  },
+  {
+    "revision": "c29ffab9b7452d289176",
+    "url": "/crm-app/static/js/runtime-main.b7e069b8.js"
+  }
+]);
