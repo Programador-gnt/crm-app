5b67b6c4a7a9ea3e361b6204ae095ec76aa5071c
programador-gnt <programador@gnt.pe>
2019-12-19 15:40:42 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crm-app/precache-manifest.3ffbefb1029f6da48be611e2cb9d2149.js"
+  "/crm-app/precache-manifest.f77bfaf9cb73d8bd4dc8c4a803657918.js"
 );
 
 self.addEventListener('message', (event) => {
